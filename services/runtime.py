fcd888555f57b5936a5450815c89921dc041ef9e
fujiken <kfujino2025@gmail.com>
2026-05-25 04:22:26 +0900

    Delete services directory

--- services/runtime.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-画面分離の共通実行入口。
-既存機能を壊さないため、当面は legacy_app を読み込む。
-"""
-import importlib
-
-def run_legacy_app():
-    return importlib.import_module("services.legacy_app")
